3c10371db98ea6f93fd7fbe3863809ffb3b25624
Moshe Yamini <moshe5745@gmail.com>
2025-02-23 08:42:24 +0200

    fix

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -18,7 +18,7 @@ Handler middleware(Handler handler) {
             <link href="./output.css" rel="stylesheet">
             <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/sort@3.x.x/dist/cdn.min.js"></script>
             <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-            <script src="./sse.js">
+            <script src="./sse.js"></script>
           </head>
           <body>
             ${navbar(TrustedHtml(await response.body()))}
